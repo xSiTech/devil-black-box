7e41aece027bf3874fe1c41854a59725876a25c2
Milan Golakiya <milangolakiya.tst@gmail.com>
2024-03-15 23:25:15 +0530

    cors added stag

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ import express from "express";
 import "dotenv";
 import routes from "./features/routes/routes.js";
 import fileUpload from "express-fileupload";
-
+import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(routes);
 
-app.use(fileUpload({
+app.use(
+  fileUpload({
     useTempFiles: true,
-}));
+  })
+);
 
 app.listen(PORT, () => {
-    console.log(`server is Started at port ${PORT}`);
+  console.log(`server is Started at port ${PORT}`);
 });
